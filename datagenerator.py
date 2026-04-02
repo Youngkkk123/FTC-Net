b37b579910763810e29fe4cdffc16fd8424db9fa
Youngkkk123 <104311745+Youngkkk123@users.noreply.github.com>
2026-04-02 14:33:22 +0800

    Update datagenerator.py

--- datagenerator.py
+++ datagenerator.py
@@ -1,15 +1,10 @@
-from torchvision import transforms
 from dataAugmentation import ImageEqualSize, Square_Generated
-import torch
-import numpy as np
 from torch.utils.data import Dataset
 import os
-import PIL
 from PIL import Image
 
 
 class Datasets_Bmode_patient_dir(Dataset):
-    '''加载超声图像和标签的数据集类'''
     def __init__(self, data_list, transform=None, device='cuda:0'):
         self.data_list = data_list
         self.transform = transform
@@ -21,12 +16,9 @@ class Datasets_Bmode_patient_dir(Dataset):
     def __getitem__(self, index):
         # 提取图像路径和标签
         patient_path, label = self.data_list[index]
-        patient_path = patient_path.replace(r'D:\project\Thyroid_classify\FTCdata', r'D:\Work\Thyroid_classify\FTC_train_data')
-        patient_path = patient_path.replace(r'D:\FTC_data\验证集合并', r'D:\Work\Thyroid_classify\FTC_train_data\external_test_new\验证集一')
-        # patient_path = patient_path.replace(r'D:\project\Thyroid_classify\test1n2', r'D:\Work\Thyroid_classify\FTC_train_data\external_test_new')      # 外部
-        # patient_path = patient_path.replace(r'D:\Work\Thyroid_classify\FTC_train_data\external_test_new', r'D:\Work\Thyroid_classify\FTC_train_data\external_test_new')
+        # patient_path = patient_path.replace('\\', '/')
 
-        label = int(label)  # 确保标签是整数类型
+        label = int(label)
 
         data = {
             'path': patient_path,
@@ -41,16 +33,10 @@ class Datasets_Bmode_roi_patient_allimg(Dataset):
         self.transform = transform
         self.device = device
         self.image_paths = []
-        self.labels = []  # 显式声明 labels 属性
+        self.labels = []
 
-        # 预先生成所有图像路径和标签
         for patient_folder_path, label in self.data_list:
-            # patient_folder_path = patient_folder_path.replace('D:\\Work\\Thyroid_classify\\', '/home/yangkai/project/Thyroid_classify/')
             # patient_folder_path = patient_folder_path.replace('\\', '/')
-
-            patient_folder_path = patient_folder_path.replace(r'D:\project\Thyroid_classify\TrainDataset_1_ljz',r'D:\Work\Thyroid_classify\FTC_train_data')
-            patient_folder_path = patient_folder_path.replace(r'D:\project\Thyroid_classify\FTCdata',r'D:\Work\Thyroid_classify\FTC_train_data')
-            patient_folder_path = patient_folder_path.replace(r'D:\project\Thyroid_classify\test1n2',r'D:\Work\Thyroid_classify\FTC_train_data\external_test')
 
             label = int(label)
             image_paths = [os.path.join(patient_folder_path, f)
